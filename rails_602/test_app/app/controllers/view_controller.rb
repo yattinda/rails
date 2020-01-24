8433638cde5c47a6291ab4f5e5d54784fb931ec8
yattinda <yattinda@gmail.com>
2020-01-24 21:04:12 +0900

    make user page

--- rails_602/test_app/app/controllers/view_controller.rb
+++ rails_602/test_app/app/controllers/view_controller.rb
@@ -8,5 +8,30 @@ def form_for
   @book = Book.new
 end
 
+def html5
+  @book = Book.new
+end
+
+def select
+  @book = Book.new(publish: "技術評論社")
+end
+
+def col_select
+  #フォーム元んｐモデルを準備
+  @book = Book.new(publish:"技術評論社")
+  #選択オプションの情報習得
+  @books = Book.select(:publish).distinct
+end
+
+def group_select
+  @review = Review.new
+  @authors = Author.all
+end
+
+def select_tag2
+  @review = Review.new
+  @authors = Author.all
+end
+
 
 end
--- rails_602/test_app/config/routes.rb
+++ rails_602/test_app/config/routes.rb
@@ -8,4 +8,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "hello/view"
   get "hello/list"
+  get "view/keyword"
+  get "view/form_tag"
+  get "view/form_for"
+  get "view/html5"
+  get "view/select"
+  get "view/col_select"
+  get "view/group_select"
+  get "view/select_tag2"
 end
